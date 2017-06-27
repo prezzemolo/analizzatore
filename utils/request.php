095febcf2116a2008dc444db8cf6bf12765aee1c
prezzemolo <git@prezzemolo.ga>
2017-06-28 08:18:08 +0900

    request - add error handling & set timeout

--- utils/request.php
+++ utils/request.php
@@ -61,14 +61,23 @@ function request (string $method, string $url, array $headers = [], string $body
   ], $headers);
   curl_setopt($curl_ch, CURLOPT_HTTPHEADER, request_assemble_curl_headers($request_headers));
 
-  // set option for getting body & header with exec
+  // set nesessary options to get header, body & catch timeout error
   curl_setopt_array($curl_ch, [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HEADER => true,
-    CURLINFO_HEADER_OUT => true
+    CURLINFO_HEADER_OUT => true,
+    CURLOPT_TIMEOUT => 5
   ]);
+
+  // set response & informations
   $response = curl_exec($curl_ch);
   $informations = curl_getinfo($curl_ch);
+
+  // error handling
+  if (curl_errno($curl_ch) !== 0) {
+    throw new Exception(curl_error($curl_ch));
+  }
+
   curl_close($curl_ch);
   $raw_header = substr($response, 0, $informations['header_size']);
   $headers = new Headers($raw_header);
