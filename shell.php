a14b9a578accacd418517bd6cbbc7d1c67f7905f
prezzemolo <git@prezzemolo.ga>
2017-06-28 07:36:23 +0900

    extractors - add canonical_extractor

--- shell.php
+++ shell.php
@@ -6,7 +6,7 @@ require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'utils', 'request.php']);
 require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'utils', 'extractors.php']);
 
 use DOMDocument;
-use function analizzatore\utils\{request, ogp_extractor, metadata_extractor};
+use function analizzatore\utils\{request, ogp_extractor, metadata_extractor, canonical_extractor};
 
 $site_addr = $argv[1] ?? 'https://prezzemolo.ga/';
 $res = request('GET', $site_addr);
@@ -20,6 +20,8 @@ if (isset($res_body_DOM_head)) {
   $meta = $res_body_DOM_head->getElementsByTagName('meta');
   var_dump(ogp_extractor($meta));
   var_dump(metadata_extractor($meta));
+  $link = $res_body_DOM_head->getElementsByTagName('link');
+  var_dump(canonical_extractor($link));
 }
 $res_info = $res['info'];
 var_dump($res_headers['Content-encoding']);
--- utils/extractors.php
+++ utils/extractors.php
@@ -10,21 +10,15 @@ function ogp_extractor ($meta_DOMNodeList) {
     * http://php.net/manual/en/class.domnode.php#domnode.props.attributes
     * note: no special reason for disuse hasElements method
     */
-   if ($meta_DOMNode->attributes === null) {
-     continue;
-   }
+   if ($meta_DOMNode->attributes === null) continue;
    // skip non OGP tag
-   if (!$meta_DOMNode->attributes->getNamedItem('property')
-    || substr($meta_DOMNode->attributes->getNamedItem('property')->textContent, 0, 3) !== 'og:') {
-     continue;
-   }
+   if (!$meta_DOMNode->hasAttribute('property')
+    || substr($meta_DOMNode->getAttribute('property'), 0, 3) !== 'og:') continue;
    // skip no content
-   if (!$meta_DOMNode->attributes->getNamedItem('content')
-    || !$meta_DOMNode->attributes->getNamedItem('content')->textContent) {
-     continue;
-   }
-   $property = substr($meta_DOMNode->attributes->getNamedItem('property')->textContent, 3);
-   $content = $meta_DOMNode->attributes->getNamedItem('content')->textContent;
+   if (!$meta_DOMNode->hasAttribute('content')
+    || !$meta_DOMNode->getAttribute('content')) continue;
+   $property = substr($meta_DOMNode->getAttribute('property'), 3);
+   $content = $meta_DOMNode->getAttribute('content');
    $ogp_tags[$property] = $content;
  }
  return $ogp_tags;
@@ -37,25 +31,35 @@ function metadata_extractor ($meta_DOMNodeList) {
    /**
     * check existance of attributes
     * http://php.net/manual/en/class.domnode.php#domnode.props.attributes
-    * note: no special reason for disuse hasElements method
     */
-   if ($meta_DOMNode->attributes === null) {
-     continue;
-   }
+   if ($meta_DOMNode->attributes === null) continue;
    // skip non metadata tag
-   if (!$meta_DOMNode->attributes->getNamedItem('name')
-    || !$meta_DOMNode->attributes->getNamedItem('name')->textContent) {
-     continue;
-   }
+   if (!$meta_DOMNode->hasAttribute('name')
+    || !$meta_DOMNode->getAttribute('name')) continue;
    // skip no content
-   if (!$meta_DOMNode->attributes->getNamedItem('content')
-    || !$meta_DOMNode->attributes->getNamedItem('content')->textContent) {
-     continue;
-   }
-   $name = $meta_DOMNode->attributes->getNamedItem('name')->textContent;
-   $content = $meta_DOMNode->attributes->getNamedItem('content')->textContent;
+   if (!$meta_DOMNode->hasAttribute('content')
+    || !$meta_DOMNode->getAttribute('content')) continue;
+   $name = $meta_DOMNode->getAttribute('name');
+   $content = $meta_DOMNode->getAttribute('content');
    $metadata[$name] = $content;
   }
   return $metadata;
+}
+
+function canonical_extractor ($link_DOMNodeList) {
+  foreach ($link_DOMNodeList as $link_DOMNode) {
+   /**
+    * check existance of attributes
+    * http://php.net/manual/en/class.domnode.php#domnode.props.attributes
+    */
+   if ($link_DOMNode->attributes === null) continue;
+   // skip no rel & href in attributes
+   if (!$link_DOMNode->hasAttribute('rel')
+    || !$link_DOMNode->hasAttribute('href')) continue;
+   // skip no canonical tag
+   if ($link_DOMNode->getAttribute('rel') !== 'canonical') continue;
+   return $link_DOMNode->getAttribute('href');
+ }
+ return null;
 }
 ?>
