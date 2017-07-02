dcdcaccfc0f7c78e2834613f949180948ec20341
prezzemolo <git@prezzemolo.ga>
2017-07-03 03:15:24 +0900

    remove WIP require_once

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ namespace analizzatore;
 require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'utils', 'request.php']);
 require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'utils', 'extractors.php']);
 require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'common', 'exceptions.php']);
-require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'processors', 'index.php']);
 
 use DOMDocument;
 use Exception;
