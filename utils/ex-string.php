40076862abdd9fcf84cb384b470b170503f5cb5b
prezzemolo <git@prezzemolo.ga>
2017-08-03 05:32:16 +0900

    [ExString] add extra encoding functions

--- utils/ex-string.php
+++ utils/ex-string.php
@@ -12,4 +12,18 @@ class ExString {
     if ($endPosision <= 0) return false;
     return mb_strpos($target, $suffix) === $endPosision;
   }
+
+  public static function list_encodings_with_alias () {
+    $list = [];
+    foreach(mb_list_encodings() as $encoding) {
+      array_push($list, $encoding);
+      $list = array_merge($list, mb_encoding_aliases($encoding));
+    }
+    return $list;
+  }
+
+  public static function check_encoding_loadable (string $encoding) {
+    $encodings = self::list_encodings_with_alias();
+    return in_array($encoding, $encodings);
+  }
 }
