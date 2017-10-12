13be3e8c7c411ce76f6c237c0099c9c55e381ff3
prezzemolo <git@prezzemolo.ga>
2017-10-13 03:11:21 +0900

    [Contants::ANALIZZATORE_VERSION] forgot to bump version

--- src/common/constants.php
+++ src/common/constants.php
@@ -3,7 +3,7 @@
 namespace analizzatore;
 
 class Constants {
-  const ANALIZZATORE_VERSION = '0.3.3';
+  const ANALIZZATORE_VERSION = '0.3.5';
   const ANALIZZATORE_UA =
     'Mozilla/5.0 (compatible; analizzatore/'
     . self::ANALIZZATORE_VERSION
