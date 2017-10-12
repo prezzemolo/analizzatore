21f5731a9955be7d80b8c3b81c08933f2372bb18
prezzemolo <git@prezzemolo.ga>
2017-10-13 08:00:17 +0900

    [robots.txt parser] some optimizations
    * skip some processes
    * if user_agent presented, skip non matched blocks

--- src/utils/robots-txt-parser.php
+++ src/utils/robots-txt-parser.php
@@ -12,23 +12,27 @@ function robots_txt_pathvalue_to_regexp (string $v): string {
 }
 
 // return 'regexp' mathcer string, to use with REQUEST_URI
-function parse_robots_txt (string $user_agent, string $body) {
+function parse_robots_txt (string $body, string $user_agent = null) {
   $f_s = explode('User-agent:', $body);
-  $s_s = array_map(function ($v) {
-    list($ua_in_ss, $content_in_ss) = preg_split('/\r?\n/', $v, 2);
-    $ua_in_ss = trim($ua_in_ss);
-    if (($ua_hash_position = strpos($ua_in_ss, '#')) !== false) {
-      // if user-agent starts with #, maybe comments (non-block)
-      if ($ua_hash_position === 0) return null;
-      // cut off comment
-      $ua_in_ss = trim(substr($ua_in_ss, 0, $ua_hash_position));
-    }
+  // drop first value, only need values related to 'User-agent'
+  array_shift($f_s);
+  $s_s = array_map(function ($v) use ($user_agent) {
+    list($ua_in_ss, $content_in_ss) = array_map('trim', preg_split('/\r?\n/', trim($v), 2));
+
+    // create user-agent matcher, only enables '*' element
+    $uam = '~' . str_replace('\*', '.*', preg_quote($ua_in_ss)) . '~';
+    // only continue with matched user-agent
+    if ($user_agent && !preg_match($uam, $user_agent)) return null;
+
     $regexps_disallow_in_ss = array();
     $regexps_allow_in_ss = array();
     foreach (preg_split('/\r?\n/', trim($content_in_ss)) as $v) {
-      list($name_in_ra_in_ss, $val_in_ra_in_ss) = array_map(function ($v) {
-        return trim($v);
-      }, explode(':', $v));
+      $line_in_content = trim($v);
+      // a line contains, skip
+      if (!trim($line_in_content)) continue;
+      // a line starts with '#', skip
+      if (strpos($line_in_content, '#') === 0) continue;
+      list($name_in_ra_in_ss, $val_in_ra_in_ss) = array_map('trim', explode(':', $line_in_content));
       // check & cut off comments
       if (($val_hash_position = strpos($val_in_ra_in_ss, '#')) !== false) {
         // cut off comment
